8e4a022ead2d05a4e98c48b3ff9f3365e18817dd
dekudekuKa <karpavovaforvalorant@gmail.com>
2025-01-15 12:52:58 +0200

    Relay + movement updates.ino

--- OREST.ino
+++ OREST.ino
@@ -19,6 +19,7 @@
 #define OFF 1
 #define ON 0
 
+
 // SBUS objects
 bfs::SbusRx sbus_rx(&Serial1);
 bfs::SbusTx sbus_tx(&Serial1);
@@ -29,11 +30,14 @@ int joystick_left_y = 0;
 int joystick_left_y_speed = 0;
 int joystick_right_x = 0;
 int joystick_right_x_speed = 0;
+int joystick_left_x = 0;
+int joystick_left_x_turn = 0;
+int speedAdjustment = 0;
 
 bool relay1State = false; // Стан реле 1 (вимкнене за замовчуванням)
 bool relay2State = false; // Стан реле 2 (вимкнене за замовчуванням)
 bool previousSwitchStateRelay1 = false; // Попередній стан для реле 1
-bool previousSwitchStateRelay2 = false; // Попередній стан для реле 2
+bool previousSwitchStateRelay2 = false;
 
 void setup() {
     Serial.begin(115200);
@@ -70,11 +74,13 @@ void loop() {
 void processJoystickInput() {
     // Отримуємо значення джойстиків
     joystick_left_y = data.ch[2];
-    joystick_left_y_speed = map(joystick_left_y, 173, 1810, 0, 255);
+    joystick_left_y_speed = map(joystick_left_y, 173, 1810, 0, 85);
     joystick_right_x = data.ch[0];
-    joystick_right_x_speed = map(joystick_right_x, 173, 1810, -255, 255);
+    joystick_right_x_speed = map(joystick_right_x, 173, 1810, -85, 85);
+    joystick_left_x = data.ch[3];
+    joystick_left_x_turn = map(joystick_left_x, 173, 1810, -80, 80);
 
-    // Перевіряємо управління рухом
+    // Перевіряємо перемикач B для визначення напрямку руху
     if (data.ch[6] < 990) {
         moveForward();
     } else if (data.ch[6] > 1000) {
@@ -84,7 +90,22 @@ void processJoystickInput() {
         Serial.println("Neutral");
     }
 
-    // Перемикання реле 1 (вгору/вниз)
+    // Перевіряємо перемикач C для зміни передачі
+    if (data.ch[7] == 173) {
+        speedAdjustment = 3; // Gear 1
+    } else if (data.ch[7] == 993) {
+        speedAdjustment = 2; // Gear 2
+    } else if (data.ch[7] == 1810) {
+        speedAdjustment = 1; // Gear 3
+    }
+
+    if (joystick_right_x_speed > 0) {
+      turnOnSpot(true);
+    } else if (joystick_right_x_speed < 0) {
+      turnOnSpot(false);
+    }
+
+
     bool currentSwitchStateRelay1 = (data.ch[4] > 993); // Верхнє положення
     toggleRelay(IN1, 0, currentSwitchStateRelay1, previousSwitchStateRelay1);
 
@@ -92,22 +113,31 @@ void processJoystickInput() {
     bool currentSwitchStateRelay2 = (data.ch[4] < 993); // Нижнє положення
     toggleRelay(IN2, 1, currentSwitchStateRelay2, previousSwitchStateRelay2);
 
+
     // Виведення дебаг-інформації
     Serial.print("Speed: ");
     Serial.print(joystick_left_y_speed);
-    Serial.print(" Turn: ");
-    Serial.println(joystick_right_x_speed);
+    Serial.print(" Turn (on spot): ");
+    Serial.print(joystick_right_x_speed);
+    Serial.print(" Turn (while driving): ");
+    Serial.println(joystick_left_x_turn);
 }
 
 void moveForward() {
-    setMotorSpeeds(joystick_left_y_speed, joystick_right_x_speed);
-    setMotorDirections(false, true); // Motor A: Forward, Motor B: Forward
+    int adjustedSpeedLeft = joystick_left_y_speed * speedAdjustment - joystick_left_x_turn;
+    int adjustedSpeedRight = joystick_left_y_speed * speedAdjustment + joystick_left_x_turn;
+
+    setMotorSpeeds(adjustedSpeedLeft, adjustedSpeedRight);
+    setMotorDirections(false, true);
     Serial.println("Move forward");
 }
 
 void moveBackward() {
-    setMotorSpeeds(joystick_left_y_speed, joystick_right_x_speed);
-    setMotorDirections(true, false); // Motor A: Backward, Motor B: Backward
+    int adjustedSpeedLeft = joystick_left_y_speed * speedAdjustment - joystick_left_x_turn;
+    int adjustedSpeedRight = joystick_left_y_speed * speedAdjustment + joystick_left_x_turn;
+
+    setMotorSpeeds(adjustedSpeedLeft, adjustedSpeedRight);
+    setMotorDirections(true, false);
     Serial.println("Move backward");
 }
 
@@ -120,9 +150,9 @@ void stopMotors() {
     analogWrite(ENB, 0);
 }
 
-void setMotorSpeeds(int baseSpeed, int turnAdjustment) {
-    int leftSpeed = constrain(baseSpeed + turnAdjustment, 0, 255);
-    int rightSpeed = constrain(baseSpeed - turnAdjustment, 0, 255);
+void setMotorSpeeds(int leftSpeed, int rightSpeed) {
+    leftSpeed = constrain(leftSpeed, 0, 255);
+    rightSpeed = constrain(rightSpeed, 0, 255);
 
     analogWrite(ENA, leftSpeed);
     analogWrite(ENB, rightSpeed);
@@ -133,6 +163,25 @@ void setMotorDirections(bool motorA_reverse, bool motorB_reverse) {
     digitalWrite(MOTORA_2, !motorA_reverse);
     digitalWrite(MOTORB_1, motorB_reverse);
     digitalWrite(MOTORB_2, !motorB_reverse);
+}
+
+void turnOnSpot(bool clockwise) {
+    int turnSpeed = abs(joystick_right_x_speed) * speedAdjustment;
+    if (clockwise) {
+        analogWrite(ENA, turnSpeed);
+        analogWrite(ENB, turnSpeed);
+        digitalWrite(MOTORA_1, LOW);
+        digitalWrite(MOTORA_2, HIGH); // Ліва гусениця вперед
+        digitalWrite(MOTORB_1, LOW);
+        digitalWrite(MOTORB_2, HIGH);
+    } else {
+        analogWrite(ENA, turnSpeed);
+        analogWrite(ENB, turnSpeed);
+        digitalWrite(MOTORA_1, HIGH);
+        digitalWrite(MOTORA_2, LOW); // Ліва гусениця назад
+        digitalWrite(MOTORB_1, HIGH);
+        digitalWrite(MOTORB_2, LOW);
+    }
 }
 
 void relay_init(void) {  
@@ -170,3 +219,4 @@ void toggleRelay(int relayPin, int relayIndex, bool currentSwitchState, bool& pr
     }
     previousSwitchState = currentSwitchState; // Оновлюємо попередній стан перемикача
 }
+
